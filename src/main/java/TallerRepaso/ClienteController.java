4afdd6328a05831aaf0d861f4e5bec7291742ff1
JFelipe98 <jfciros@uqvirtual.edu.co>
2023-08-14 19:52:48 -0500

    asignación de nombres a los elementos en la GUI

--- src/main/java/TallerRepaso/ClienteController.java
+++ src/main/java/TallerRepaso/ClienteController.java
@@ -1,16 +1,32 @@
 package TallerRepaso;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 
 public class ClienteController {
 
     @FXML
-    private Label welcomeText;
+    public Label label_Nombre;
+    public Label label_Apellido;
+    public Label label_ID;
+    public Label label_Telefono;
+    public Label label_Email;
+    public Label label_TipoPersona;
+    public Label label_FechaNacimiento;
+    public DatePicker datePicker_Fecha;
+    public TextField textField_Nombre;
+    public TextField textField_Apellido;
+    public TextField textField_ID;
+    public TextField textField_Telefono;
+    public TextField textField_Email;
+    public RadioButton radioButton_Natural;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void radioButtonSelected() {
+        label_Nombre.setText("Welcome to JavaFX Application!");
     }
 }
