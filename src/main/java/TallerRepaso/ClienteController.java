e8fef81388416666233d4089bc8d0bc67d93aedf
JFelipe98 <jfciros@uqvirtual.edu.co>
2023-08-14 19:14:13 -0500

    Implementación GUI cliente

--- src/main/java/TallerRepaso/ClienteController.java
+++ src/main/java/TallerRepaso/ClienteController.java
@@ -1,4 +1,16 @@
 package TallerRepaso;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
 
 public class ClienteController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
